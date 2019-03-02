4ebcdfcc0db1f4ad4d70512408511121b2a0abbf
ImSythn <jgv.loon@gmail.com>
2019-03-02 03:31:05 +0100

    feeling and background art start

--- lib/UI/happiness_button.dart
+++ lib/UI/happiness_button.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HappinessButton extends StatelessWidget{
-  string happiness;
+  String happiness;
+  bool clicked;
 
-  HappinessButton(this.happiness);
+  HappinessButton(this.happiness, this.clicked);
 
   @override
-
+  Widget build(BuildContext context) {
+    if(happiness == 'D:'){
+      return InkWell(
+        child: Icon(Icons.mood_bad,
+          size: 100.0,
+          color: Colors.black)
+      );
+    }
+    if(happiness == ':|'){
+      return InkWell(
+          child: Icon(Icons.face,
+            size: 100.0,
+            color: Colors.black)
+      );
+    }
+    if(happiness == ':D'){
+      return InkWell(
+          child: Icon(Icons.mood,
+            size: 100.0,
+            color: Colors.black)
+      );
+    }
+    return null;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../UI/happiness_button.dart';
+import 'UI/happiness_button.dart';
+
+import 'CustomClippers/custom_shape_clipper.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -8,6 +10,60 @@ void main() {
   ));
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          HomeScreenArt(),
+          HomeScreenButtons()
+        ],
+      ),
+    );
+  }
+}
 
+
+class HomeScreenButtons extends StatefulWidget{
+  @override
+  _HomeScreenButtonsState createState() => _HomeScreenButtonsState();
+}
+
+class _HomeScreenButtonsState extends State<HomeScreenButtons>{
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new HappinessButton('D:', false),
+        new HappinessButton(':|', false),
+        new HappinessButton(':D', false),
+      ],
+    );
+  }
+}
+
+
+class HomeScreenArt extends StatefulWidget{
+  @override
+  _HomeScreenArtState createState() => _HomeScreenArtState();
+}
+
+class _HomeScreenArtState extends State<HomeScreenArt>{
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: CustomShapeClipper(),
+          child: Container(
+            alignment: AlignmentDirectional.bottomStart,
+            height: 500.0,
+            color: Colors.lightBlue,
+          ),
+        ),
+      ],
+    );
+  }
 }
