b5e7e591ecb5e348e979b62ee96eb133bb90f276
ImSythn <jgv.loon@gmail.com>
2019-03-11 02:29:36 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,137 @@
 import 'package:flutter/material.dart';
 
-import 'UI/happiness_button.dart';
+void main() => runApp(HomePage()); //=> Shortcut for running one single line of code.
 
-import 'CustomClippers/custom_shape_clipper.dart';
-
-void main() {
-  runApp(new MaterialApp(
-    home: new HomePage()
-  ));
-}
-
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Column(
-        children: <Widget>[
-          HomeScreenArt(),
-          HomeScreenButtons()
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Rate My Vehicle'),
+          backgroundColor: Colors.lightBlue
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 50),
+              VehicleID(),
+              SizedBox(height: 70),
+              QRScanner(),
+              SizedBox(height: 70),
+              RatingButtons(),
+              SizedBox(height: 70),
+              MessageField()
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-
-class HomeScreenButtons extends StatefulWidget{
+class VehicleID extends StatefulWidget {
   @override
-  _HomeScreenButtonsState createState() => _HomeScreenButtonsState();
+  _VehicleIDState createState() => _VehicleIDState();
 }
 
-class _HomeScreenButtonsState extends State<HomeScreenButtons>{
+class _VehicleIDState extends State<VehicleID>{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(Icons.train, size: 200, color: Colors.lightBlue,),
+        Text('Vehicle ID: ')
+      ],
+    );
+  }
+}
+
+class QRScanner extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Icon(Icons.camera_alt,size: 50, color: Colors.black),
+        Text('Scan your vehicle')
+      ],
+    );
+  }
+}
+
+class RatingButtons extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _RatingButtons();
+  }
+}
+
+class _RatingButtons extends State<RatingButtons> {
+  List<Color> _buttonColor = [Colors.black12, Colors.black12, Colors.black12];
+  List<bool> pressed = [false, false, false];
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        new HappinessButton('D:', false),
-        new HappinessButton(':|', false),
-        new HappinessButton(':D', false),
-      ],
-    );
-  }
-}
-
-
-class HomeScreenArt extends StatefulWidget{
-  @override
-  _HomeScreenArtState createState() => _HomeScreenArtState();
-}
-
-class _HomeScreenArtState extends State<HomeScreenArt>{
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ClipPath(
-          clipper: CustomShapeClipper(),
-          child: Container(
-            alignment: AlignmentDirectional.bottomStart,
-            height: 500.0,
-            color: Colors.lightBlue,
-          ),
+        IconButton(
+          iconSize: 100,
+          icon: Icon(Icons.mood_bad, size: 100, color: _buttonColor[0],),
+          onPressed: (){
+            setState((){
+              if(pressed[0] == false){
+                pressed[0] = true;
+                pressed[1] = false;
+                pressed[2] = false;
+              }
+              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
+              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
+              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
+            });
+          }  
         ),
+        IconButton(
+          iconSize: 100,
+          icon: Icon(Icons.sentiment_neutral, size: 100, color: _buttonColor[1],),
+          onPressed: (){
+            setState((){
+              if(pressed[1] == false){
+                pressed[0] = false;
+                pressed[1] = true;
+                pressed[2] = false;
+              }
+              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
+              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
+              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
+            });
+          }  
+        ),
+        IconButton(
+          iconSize: 100,
+          icon: Icon(Icons.mood, size: 100, color: _buttonColor[2],),
+          onPressed: (){
+            setState((){
+              if(pressed[2] == false){
+                pressed[0] = false;
+                pressed[1] = false;
+                pressed[2] = true;
+              }
+              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
+              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
+              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
+            });
+          }  
+        )
       ],
     );
   }
 }
+
+class MessageField extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return TextField();
+  }
+}
+
+
