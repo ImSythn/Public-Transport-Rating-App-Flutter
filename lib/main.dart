7940d208bfdf7e5b9568df68a4303226c47c8fc1
ImSythn <jgv.loon@gmail.com>
2019-03-18 15:40:42 +0100

    button for martin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomePage()); //=> Shortcut for running one single line of code.
+void main() =>
+    runApp(HomePage()); //=> Shortcut for running one single line of code.
 
 class HomePage extends StatelessWidget {
   @override
@@ -8,9 +9,7 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Rate My Vehicle'),
-          backgroundColor: Colors.lightBlue
-        ),
+            title: Text('Rate My Vehicle'), backgroundColor: Colors.lightBlue),
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
@@ -35,35 +34,37 @@ class VehicleID extends StatefulWidget {
   _VehicleIDState createState() => _VehicleIDState();
 }
 
-class _VehicleIDState extends State<VehicleID>{
+class _VehicleIDState extends State<VehicleID> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Icon(Icons.train, size: 200, color: Colors.lightBlue,),
+        Icon(
+          Icons.train,
+          size: 200,
+          color: Colors.lightBlue,
+        ),
         Text('Vehicle ID: ')
       ],
     );
   }
 }
 
-class QRScanner extends StatelessWidget{
+class QRScanner extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Icon(Icons.camera_alt,size: 50, color: Colors.black),
+        Icon(Icons.camera_alt, size: 50, color: Colors.black),
         Text('Scan your vehicle')
       ],
     );
   }
 }
 
-class RatingButtons extends StatefulWidget{
+class RatingButtons extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _RatingButtons();
-  }
+  State<StatefulWidget> createState() => _RatingButtons();
 }
 
 class _RatingButtons extends State<RatingButtons> {
@@ -75,63 +76,101 @@ class _RatingButtons extends State<RatingButtons> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         IconButton(
-          iconSize: 100,
-          icon: Icon(Icons.mood_bad, size: 100, color: _buttonColor[0],),
-          onPressed: (){
-            setState((){
-              if(pressed[0] == false){
-                pressed[0] = true;
-                pressed[1] = false;
-                pressed[2] = false;
-              }
-              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
-              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
-              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
-            });
-          }  
-        ),
+            iconSize: 100,
+            icon: Icon(
+              Icons.mood_bad,
+              size: 100,
+              color: _buttonColor[0],
+            ),
+            onPressed: () {
+              setState(() {
+                if (pressed[0] == false) {
+                  pressed[0] = true;
+                  pressed[1] = false;
+                  pressed[2] = false;
+                }
+                pressed[0] == true
+                    ? _buttonColor[0] = Colors.black
+                    : _buttonColor[0] = Colors.black12;
+                pressed[1] == true
+                    ? _buttonColor[1] = Colors.black
+                    : _buttonColor[1] = Colors.black12;
+                pressed[2] == true
+                    ? _buttonColor[2] = Colors.black
+                    : _buttonColor[2] = Colors.black12;
+              });
+            }),
         IconButton(
-          iconSize: 100,
-          icon: Icon(Icons.sentiment_neutral, size: 100, color: _buttonColor[1],),
-          onPressed: (){
-            setState((){
-              if(pressed[1] == false){
-                pressed[0] = false;
-                pressed[1] = true;
-                pressed[2] = false;
-              }
-              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
-              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
-              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
-            });
-          }  
-        ),
+            iconSize: 100,
+            icon: Icon(
+              Icons.sentiment_neutral,
+              size: 100,
+              color: _buttonColor[1],
+            ),
+            onPressed: () {
+              setState(() {
+                if (pressed[1] == false) {
+                  pressed[0] = false;
+                  pressed[1] = true;
+                  pressed[2] = false;
+                }
+                pressed[0] == true
+                    ? _buttonColor[0] = Colors.black
+                    : _buttonColor[0] = Colors.black12;
+                pressed[1] == true
+                    ? _buttonColor[1] = Colors.black
+                    : _buttonColor[1] = Colors.black12;
+                pressed[2] == true
+                    ? _buttonColor[2] = Colors.black
+                    : _buttonColor[2] = Colors.black12;
+              });
+            }),
         IconButton(
-          iconSize: 100,
-          icon: Icon(Icons.mood, size: 100, color: _buttonColor[2],),
-          onPressed: (){
-            setState((){
-              if(pressed[2] == false){
-                pressed[0] = false;
-                pressed[1] = false;
-                pressed[2] = true;
-              }
-              pressed[0] == true ? _buttonColor[0] = Colors.black : _buttonColor[0] = Colors.black12;
-              pressed[1] == true ? _buttonColor[1] = Colors.black : _buttonColor[1] = Colors.black12;
-              pressed[2] == true ? _buttonColor[2] = Colors.black : _buttonColor[2] = Colors.black12;
-            });
-          }  
-        )
+            iconSize: 100,
+            icon: Icon(
+              Icons.mood,
+              size: 100,
+              color: _buttonColor[2],
+            ),
+            onPressed: () {
+              setState(() {
+                if (pressed[2] == false) {
+                  pressed[0] = false;
+                  pressed[1] = false;
+                  pressed[2] = true;
+                }
+                pressed[0] == true
+                    ? _buttonColor[0] = Colors.black
+                    : _buttonColor[0] = Colors.black12;
+                pressed[1] == true
+                    ? _buttonColor[1] = Colors.black
+                    : _buttonColor[1] = Colors.black12;
+                pressed[2] == true
+                    ? _buttonColor[2] = Colors.black
+                    : _buttonColor[2] = Colors.black12;
+              });
+            })
       ],
     );
   }
 }
 
-class MessageField extends StatelessWidget{
+class MessageField extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return TextField();
-  }
+  State<StatefulWidget> createState() => _MessageField();
 }
 
-
+class _MessageField extends State<MessageField> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          iconSize: 100,
+          icon: Icon(Icons.camera_alt),
+        )
+      ]
+    );
+  }
+}
